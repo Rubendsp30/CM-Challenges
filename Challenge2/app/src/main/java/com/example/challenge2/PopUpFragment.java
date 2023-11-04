e72b3952510d520ab6e799e5b993b0cfc74dd49f
Rubendsp30 <72309163+Rubendsp30@users.noreply.github.com>
2023-11-04 18:47:11 +0000

    Cleaning code PopUpFragment

--- Challenge2/app/src/main/java/com/example/challenge2/PopUpFragment.java
+++ Challenge2/app/src/main/java/com/example/challenge2/PopUpFragment.java
@@ -2,13 +2,10 @@ package com.example.challenge2;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -26,62 +23,68 @@ public class PopUpFragment extends DialogFragment {
     private String title;
     private String docId;
 
+    // This method is called to create and configure the dialog when this fragment is displayed.
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Create the dialog as you did before
-
+        // Create an AlertDialog instance and set its appearance style.
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.CustomDialog);
         LayoutInflater inflater = requireActivity().getLayoutInflater();
         View dialogView = inflater.inflate(R.layout.note_pop_up, null);
 
+        // Find buttons in the dialog layout.
         Button editPopUpButton = dialogView.findViewById(R.id.editPopUpButton);
         Button erasePopUpButton = dialogView.findViewById(R.id.erasePopUpButton);
 
+        // Get arguments passed to this fragment.
         Bundle args = getArguments();
         if (args != null) {
             loggedInUser = (User) args.getSerializable("loggedInUser");
-            title = (String) args.getSerializable("title");
-            docId = (String) args.getSerializable("docId");
+            title = args.getString("title");
+            docId = args.getString("docId");
         }
 
+        // Set the view for the AlertDialog.
         builder.setView(dialogView);
 
-        erasePopUpButton.setOnClickListener( (v)-> eraseNote());
+        // Set a click listener for the erase button.
+        erasePopUpButton.setOnClickListener((v) -> eraseNote());
 
-        // Show the overlay when the dialog is displayed
+        // Show an overlay in the parent fragment when the dialog is displayed.
         ((ListNotesFragment) getParentFragment()).showOverlay();
 
-        return builder.create();
+        return builder.create(); // Return the configured dialog.
     }
 
+    // This method is called when the dialog is dismissed.
     @Override
-    public void onDismiss(DialogInterface dialog) {
+    public void onDismiss(@NonNull DialogInterface dialog) {
         super.onDismiss(dialog);
 
-        // Hide the overlay when the dialog is dismissed
+        // Hide the overlay in the parent fragment when the dialog is dismissed.
         ((ListNotesFragment) getParentFragment()).hideOverlay();
     }
 
-    void eraseNote(){
+    // Method to delete a note.
+    void eraseNote() {
         DocumentReference documentReference;
         documentReference = FirebaseFirestore.getInstance().collection("notes").document(loggedInUser.getUsername()).collection("my_notes").document(docId);
 
         documentReference.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     Toast.makeText(getContext(), "Note deleted successfully", Toast.LENGTH_SHORT).show();
-                    refreshAdapter();
-                    dismiss();
-                }else{
+                    refreshAdapter(); // Refresh the adapter after deleting a note.
+                    dismiss(); // Dismiss the dialog.
+                } else {
                     Toast.makeText(getContext(), "Failed while deleting note", Toast.LENGTH_SHORT).show();
                 }
-
             }
         });
     }
 
+    // Method to refresh the FirestoreRecyclerAdapter after deleting a note.
     void refreshAdapter() {
         FirestoreRecyclerAdapter<Note, NotesAdapter.NotesViewHolder> adapter = ((ListNotesFragment) getParentFragment()).getNotesAdapter();
         if (adapter != null) {
