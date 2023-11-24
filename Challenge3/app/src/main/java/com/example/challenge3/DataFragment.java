21ce0b87533da61bbedf67b302a2851b997beab0
Rubendsp30 <72309163+Rubendsp30@users.noreply.github.com>
2023-11-24 20:29:49 +0000

    Create UI

---
+++ Challenge3/app/src/main/java/com/example/challenge3/DataFragment.java
@@ -0,0 +1,31 @@
+package com.example.challenge3;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.anychart.AnyChartView;
+
+
+
+public class DataFragment extends Fragment {
+    private AnyChartView anyChartView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.data_fragment, container, false);
+
+        return view;
+    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+
+}
--- Challenge3/app/src/main/java/com/example/challenge3/MainActivity.java
+++ Challenge3/app/src/main/java/com/example/challenge3/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.challenge3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main);
+
+        // Load the initial LoginFragment when the activity is created.
+        loadFragment(new DataFragment(), "data_fragment");
+    }
+
+    @Override
+    public void replaceFragment(Fragment fragment) {
+        // Replace the current fragment with the provided fragment.
+        if (fragment != null) {
+            loadFragment(fragment, fragment.toString());
+        }
+    }
+
+    private void loadFragment(Fragment fragment, String tag) {
+        // Load the specified fragment into the fragment container view
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragmentContainerView, fragment, tag)
+                    .commit();
+        }
     }
 }
